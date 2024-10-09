4d38838462c4109b836c447f432f72db8fd71c88
ricaldimd15@gmail.com <ricaldimd15@gmail.com>
2024-10-09 15:24:08 -0400

    final

--- newrelic.js
+++ newrelic.js
@@ -1,11 +1,11 @@
 'use strict';
 
-/** New Relic agent configuration. */
+
 exports.config = {
   app_name: ['Wingz'],
   license_key: '1a1e5db391c2dcbaa8b0739dbf31fc0dFFFFNRAL',
   logging: {
-    level: 'info', // Puedes cambiar a 'debug' para más información
+    level: 'info', 
   },
-  // Otras configuraciones según tus necesidades
+  
 };
